cfb2f0d53c0b73db401e490f9fa834502968c9db
Joshua Franz <jfranz@zoox.com>
2016-08-11 16:49:10 -0700

    bluh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+echo 'source ~/joshfig/josh_bashrc.sh' >> ~/.bashrc
 echo 'source ~/joshfig/josh.vim' >> ~/.vimrc
 echo '$include ~/joshfig/inputrc' >> ~/.inputrc
 echo 'source-file ~/joshfig/tmux.conf' >> ~/.tmux.conf
