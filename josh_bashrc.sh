7f88eb0a04ff233f15d585a61366572ade921986
Joshua Franz <jufranz@umich.edu>
2018-06-11 07:36:56 -0700

    changed PS1 to use hostname (useful now that I have so many machines), added base16 configuration and tmux coloring

--- josh_bashrc.sh
+++ josh_bashrc.sh
@@ -6,5 +6,13 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]/"
 }
 
-#export PS1='\[\033[0;32m\]\u@jfranz-argo\[\033[0;36m\] \w\[\033[31m\] $(parse_git_branch)\[\033[0;36m\]$\[\033[00m\] '
-export PS1='\[\033[0;32m\]\u@jfranz-argo\[\033[0;36m\] \w\[\033[31m\] \[\033[0;36m\]$\[\033[00m\] '
+#export PS1='\[\033[0;32m\]\u@\H\[\033[0;36m\] \w\[\033[31m\] $(parse_git_branch)\[\033[0;36m\]$\[\033[00m\] '
+export PS1='\[\033[0;32m\]\u@\H\[\033[0;36m\] \w\[\033[31m\] \[\033[0;36m\]$\[\033[00m\] '
+
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+base16_default-dark
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ echo 'source ~/joshfig/josh.vim' >> ~/.vimrc
 echo '$include ~/joshfig/inputrc' >> ~/.inputrc
 echo 'source-file ~/joshfig/tmux.conf' >> ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
