442159ef8e18ac5ba56a9c02614b6363337be2ca
Dan Barrowman <dbarrowman@argo.ai>
2020-06-07 16:55:10 -0700

    fixed setup.sh to work better for tmux

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 bind -f $DIRNAME/inputrc
-alias tmux="tmux -f $DIRNAME/tmux.conf"
+alias tmux="tmux -f $DIRNAME/tmux_bashrc.conf"
 alias vim="vim -u $DIRNAME/josh.vim"
+source $DIRNAME/josh_bashrc.sh
