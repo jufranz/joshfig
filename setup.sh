db49ee30278b296fac465f8167479398226341a1
Joshua Franz <jufranz@umich.edu>
2020-06-07 16:19:20 -0700

    renamed setup to install and created a setup script for single time use

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
-append_if_not_found () {
-  grep -qF "$1" $2 && echo "$2 already contains line '$1'" || echo "$1" >> $2
-}
-append_if_not_found 'source ~/joshfig/josh_bashrc.sh' ~/.bashrc
-append_if_not_found 'source ~/joshfig/josh.vim' ~/.vimrc
-append_if_not_found '$include ~/joshfig/inputrc' ~/.inputrc
-append_if_not_found 'source-file ~/joshfig/tmux.conf' ~/.tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+#!/bin/sh
+DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+bind -f $DIRNAME/inputrc
+alias tmux="tmux -f $DIRNAME/tmux.conf"
+alias vim="vim -u $DIRNAME/josh.vim"
