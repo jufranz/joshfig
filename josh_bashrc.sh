521a8c98e46a3b46210279c27e74fedd6c2b70e5
Joshua Franz <joshua.u.franz@gmail.com>
2022-10-19 15:09:15 -0700

    fixed an issue with base16_shell by re-copying the bashrc code from github and moving to the top of bashrc

--- josh_bashrc.sh
+++ josh_bashrc.sh
@@ -2,6 +2,13 @@
 # https://stackoverflow.com/questions/23669521/scp-to-remote-host-without-executing-bash-profile-file-on-that-host 
 [[ $- == *i* ]] || return
 
+# Base16 Shell
+BASE16_SHELL="$HOME/.config/base16-shell/"
+[ -n "$PS1" ] && \
+	[ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+	    source "$BASE16_SHELL/profile_helper.sh"
+base16_default-dark
+
 alias ll='ls -la'
 function parse_git_dirty {
   [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
@@ -22,12 +29,5 @@ RESET="\[\033[0m\]"
 #export PS1='\[\033[0;32m\]\u@\H\[\033[0;36m\] \w\[\033[31m\] \[\033[0;36m\]$\[\033[00m\] '
 #export PS1="${GREEN}\u@\H${TURQUOISE} \w $ ${RESET}"
 export PS1="${BLUE}\u@\H ${RED}>${YELLOW}>${GREEN}> ${TURQUOISE}\w/\n${GREEN}$ ${RESET}"
-
-# Base16 Shell
-BASE16_SHELL="$HOME/.config/base16-shell/"
-[ -n "$PS1" ] && \
-    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-        eval "$("$BASE16_SHELL/profile_helper.sh")"
-base16_default-dark
 
 export EDITOR='vim'
